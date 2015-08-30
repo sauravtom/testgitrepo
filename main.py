8d7238fe6cee986398f59c41a10a24c0b9ca3bdd
Saurav Tomar <stomatrix@gmail.com>
2015-08-30 08:02:47 -0400

    lots of changes

--- main.py
+++ main.py
@@ -51,15 +51,31 @@ def main():
     sendVideo(message.text,message.chat_id)
     addToRepliedMessages(message)
 
-def sendVideo(query,chat_id='724255'):
+def sendVideo(query,chat_id='724255',HD=False):
     title,id=srch(query)
     epoch = str(int(time.time()*100))
-    os.system("youtube-dl -f 13 %s  -o 'video%s.%%(ext)s'"%(id,epoch))
-    video = open('/Users/saurav/Desktop/jalebiBot/video%s.3gp'%epoch, 'rb')
+    if HD:
+        os.system("youtube-dl %s  -o 'video%s.%%(ext)s'"%(id,epoch))
+    else:
+        os.system("youtube-dl -f 13 %s  -o 'video%s.%%(ext)s'"%(id,epoch))
+    video = open('/home/saurav/jalebibot/video%s.3gp'%epoch, 'rb')
     tb.send_message(chat_id,"Title: '%s'"%title)
     tb.send_chat_action(chat_id, 'record_video')
     tb.send_video(chat_id, video)
     os.system("rm video%s.3gp"%epoch)
+
+def sendSong(query,chat_id='724255',HD=False):
+    title,id=srch(query)
+    epoch = str(int(time.time()*100))
+    if HD:
+        os.system("youtube-dl --extract-audio --audio-format mp3  %s  -o 'song%s.%%(ext)s'"%(id,epoch))
+    else:
+        os.system("youtube-dl -f 13 --extract-audio --audio-format mp3  %s  -o 'song%s.%%(ext)s'"%(id,epoch))
+    tb.send_message(chat_id,"Title: '%s'"%title)
+    audio = open('/home/saurav/jalebibot/song%s.mp3'%epoch, 'rb')
+    tb.send_chat_action(chat_id, 'upload_audio')
+    tb.send_audio(chat_id, audio)
+    os.system("rm song%s.mp3"%epoch)
 
 def test2():
     result = ydl.extract_info(
@@ -70,11 +86,41 @@ def test2():
 
 @tb.message_handler(commands=['download'])
 def send_welcome(message):
+    tb.send_message(message.chat.id, "YO %s, here's your video for '%s' (/help for more info)"%(message.from_user.first_name,message.text[10:]))
+    #tb.reply_to(message, "YO %s, here's your video for '%s'"%(message.from_user.first_name,message.text[10:]))
+    print message.text
+    #print dir(message.chat)
+    if 'HD' in message.text:
+        sendVideo(message.text[10:],message.chat.id,HD=True)
+    else:
+        sendVideo(message.text[10:],message.chat.id,HD=False)
+
+@tb.message_handler(commands=['dwd'])
+def send_video(message):
     tb.send_message(message.chat.id, "YO %s, here's your video for '%s'"%(message.from_user.first_name,message.text[10:]))
     #tb.reply_to(message, "YO %s, here's your video for '%s'"%(message.from_user.first_name,message.text[10:]))
     print message.text
     #print dir(message.chat)
-    sendVideo(message.text[10:],message.chat.id)
+    if 'HD' in message.text:
+        sendVideo(message.text[10:].replace("HD",""),message.chat.id,HD=True)
+    else:
+        sendVideo(message.text[10:],message.chat.id,HD=False)
+
+@tb.message_handler(commands=['song'])
+def send_welcome(message):
+    tb.send_message(message.chat.id, "YO %s, here's your song for '%s' (/help for more info)"%(message.from_user.first_name,message.text[6:]))
+    #tb.reply_to(message, "YO %s, here's your video for '%s'"%(message.from_user.first_name,message.text[10:]))
+    print message.text
+    #print dir(message.chat)
+    if 'HD' in message.text:
+        sendSong(message.text[6:].replace("HD",""),message.chat.id,HD=True)
+    else:
+        sendSong(message.text[6:],message.chat.id,HD=False)
+
+@tb.message_handler(commands=['help'])
+def send_welcome(message):
+    tb.send_message(message.chat.id, "YO %s, here's the complete list of commands. \n\n /download <keyword> | download video matching the keyword \
+    \n\n /dwd <keyword> | same as above \n\n /song <keyword> |download mp3 \n\n Adding 'HD' anywhere in the keyword will download its HD version."%(message.from_user.first_name))
 
 def start():
     tb.polling()
@@ -82,6 +128,7 @@ def start():
         time.sleep(5000)
 
 if __name__ == '__main__':
+    #sendSong("sun raha hai na tu")
     start()
     #sendVideo("food")
     #test()
